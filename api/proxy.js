fe13c864698e514215ab7e51c6fbbfc0ce326a3b
Suan <suan.tay.job@gmail.com>
2025-06-13 14:08:39 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -48,7 +48,8 @@ export default async function handler(req, res) {
     }
 
     const data = await response.json();
-
+    console.log("🔎 Données renvoyées à GPT :", JSON.stringify(data, null, 2));
+    
     return res.status(200).json({
       success: true,
       data: data,
